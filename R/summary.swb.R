c95aad70c6838adc8b734bb1a4588cc97d0e9907
Miquel De Cáceres <miquelcaceres@gmail.com>
2018-04-26 20:04:24 +0200

    new rmarkdown docs

--- R/summary.swb.R
+++ R/summary.swb.R
@@ -3,15 +3,19 @@ summary.swb<-function(object, freq="years", output="DailyBalance", FUN=sum,...){
   ndaysTotal = length(dates)
   date.factor = cut(dates, breaks=freq)
   if(output=="DailyBalance") OM = object$DailyBalance
+  else if(output=="PlantLAI") OM = object$PlantLAI
+  else if(output=="PlantPsi") OM = object$PlantPsi
   else if(output=="PlantStress") OM = object$PlantStress
   else if(output=="PlantTranspiration") OM = object$PlantTranspiration
+  else if(output=="PlantPhotosynthesis") OM = object$PlantPhotosynthesis
   else if(output=="SoilWaterBalance") OM = object$SoilWaterBalance
   else if(output=="TemperatureBalance") OM = object$TemperatureBalance
   
   ncoh = ncol(OM)
   M = data.frame(matrix(0,nrow=length(levels(date.factor)), ncol=ncol(OM)))
-  rownames(M)<-levels(date.factor)
-  colnames(M)<-names(OM)
+  row.names(M)<-levels(date.factor)
+  if(class(OM)=="matrix") names(M)<-colnames(OM)
+  else if(class(OM)=="data.frame") names(M)<-names(OM)
   for(c in 1:ncol(OM)){
     M[,c] <-tapply(OM[,c], INDEX=date.factor,FUN=FUN)
   }
