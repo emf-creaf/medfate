844a31db34b11d3b997e0cd970ae2852b837826f
Miquel De Cáceres <miquelcaceres@gmail.com>
2018-04-27 16:25:50 +0200

    upgrade summary functions

--- R/summary.growth.R
+++ R/summary.growth.R
@@ -1,9 +1,17 @@
-summary.growth<-function(object, freq="years", output="DailyBalance", FUN=sum,...){  
+summary.growth<-function(object, freq="years", output="DailyBalance", FUN=sum, bySpecies = FALSE, ...){  
   dates = as.Date(rownames(object$DailyBalance))
   ndaysTotal = length(dates)
   date.factor = cut(dates, breaks=freq)
   OM = object[[output]]
 
+  #Aggregate results by species
+  if(bySpecies) {
+    lai1 = t(apply(object$PlantLAI,1, tapply, object$cohorts$Name, sum))
+    m1 = t(apply(object$PlantLAI * OM,1, tapply, object$cohorts$Name, sum))
+    OM = m1/lai1
+    OM[lai1==0] = NA
+  } 
+  
   ncoh = ncol(OM)
   M = data.frame(matrix(0,nrow=length(levels(date.factor)), ncol=ncol(OM)))
   rownames(M)<-levels(date.factor)
--- R/summary.swb.R
+++ R/summary.swb.R
@@ -1,16 +1,40 @@
-summary.swb<-function(object, freq="years", output="DailyBalance", FUN=sum,...){  
+summary.swb<-function(object, freq="years", output="DailyBalance", FUN=sum, bySpecies = FALSE, ...){  
   dates = as.Date(rownames(object$DailyBalance))
   ndaysTotal = length(dates)
   date.factor = cut(dates, breaks=freq)
+  speciesOutput = FALSE
   if(output=="DailyBalance") OM = object$DailyBalance
-  else if(output=="PlantLAI") OM = object$PlantLAI
-  else if(output=="PlantPsi") OM = object$PlantPsi
-  else if(output=="PlantStress") OM = object$PlantStress
-  else if(output=="PlantTranspiration") OM = object$PlantTranspiration
-  else if(output=="PlantPhotosynthesis") OM = object$PlantPhotosynthesis
   else if(output=="SoilWaterBalance") OM = object$SoilWaterBalance
   else if(output=="TemperatureBalance") OM = object$TemperatureBalance
+  else if(output=="PlantLAI") {
+    OM = object$PlantLAI
+    speciesOutput = TRUE
+  } else if(output=="PlantPsi") {
+    OM = object$PlantPsi
+    speciesOutput = TRUE
+  }
+  else if(output=="PlantStress") {
+    OM = object$PlantStress
+    speciesOutput = TRUE
+  }
+  else if(output=="PlantTranspiration") {
+    OM = object$PlantTranspiration
+    speciesOutput = TRUE
+  }
+  else if(output=="PlantPhotosynthesis") {
+    OM = object$PlantPhotosynthesis
+    speciesOutput = TRUE
+  }
+
+  #Aggregate results by species
+  if(speciesOutput && bySpecies) {
+    lai1 = t(apply(object$PlantLAI,1, tapply, object$cohorts$Name, sum))
+    m1 = t(apply(object$PlantLAI * OM,1, tapply, object$cohorts$Name, sum))
+    OM = m1/lai1
+    OM[lai1==0] = NA
+  } 
   
+  #Perform summary at the desired temporal scale
   ncoh = ncol(OM)
   M = data.frame(matrix(0,nrow=length(levels(date.factor)), ncol=ncol(OM)))
   row.names(M)<-levels(date.factor)
