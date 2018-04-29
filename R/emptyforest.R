598b53f1d47ab1d886ad115cfd52c0719872e132
miquelcaceres <miquelcaceres@gmail.com>
2018-04-29 18:10:33 +0200

    update emptyforest

--- R/emptyforest.R
+++ R/emptyforest.R
@@ -1,13 +1,13 @@
-emptyforest<-function(ID="", patchsize=10000) {
+emptyforest<-function(ID="", patchsize=10000, ntree = 0, nshrub = 0) {
   l = list()
   l$ID = ID
   l$patchsize=10000
-  l$treeData = data.frame(Species=numeric(0),DBH=numeric(0), 
-                         Height=numeric(0), N=numeric(0),
-                         Z50 = numeric(0), Z95=numeric(0))
-  l$shrubData = data.frame(Species=numeric(0), Height=numeric(0), 
-                          Cover = numeric(0), Z = numeric(0))
-  l$seedBank = data.frame(Species=numeric(0),Abundance=numeric(0))
+  l$treeData = data.frame(Species=numeric(ntree),DBH=numeric(ntree), 
+                         Height=numeric(ntree), N=numeric(ntree),
+                         Z50 = numeric(ntree), Z95=numeric(ntree))
+  l$shrubData = data.frame(Species=numeric(nshrub), Height=numeric(nshrub), 
+                          Cover = numeric(nshrub), Z = numeric(nshrub))
+  l$seedBank = data.frame(Species=numeric(ntree+nshrub),Abundance=numeric(ntree+nshrub))
   l$herbCover = 0;
   l$herbHeight = 0;
   class(l)<-c("forest","list")
