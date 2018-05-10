6d2a226c05600754a05b0fe7885a960c9eb2f4ac
Miquel De Cáceres <miquelcaceres@gmail.com>
2018-05-10 19:17:53 +0200

    new vignette

--- R/plot.growth.R
+++ R/plot.growth.R
@@ -1,3 +1,4 @@
-plot.growth<-function(x, type="PET_Precipitation", yearAxis=FALSE, xlim = NULL, ylim=NULL, xlab=NULL, ylab=NULL,...) {
-  plot.swb(x,type, yearAxis, xlim, ylim, xlab, ylab, ...)
+plot.growth<-function(x, type="PET_Precipitation", bySpecies = FALSE, 
+                      yearAxis=FALSE, xlim = NULL, ylim=NULL, xlab=NULL, ylab=NULL,...) {
+  plot.swb(x,type, bySpecies, yearAxis, xlim, ylim, xlab, ylab, ...)
 }
