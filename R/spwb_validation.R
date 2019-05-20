812b1ac6c46b6249d0340057055aaa2007bb33c0
Miquel De Cáceres <miquelcaceres@gmail.com>
2019-05-20 13:03:35 +0200

    validation ggplots

--- R/spwb_validation.R
+++ R/spwb_validation.R
@@ -1,5 +1,25 @@
-spwb_validation<-function(x, measuredData, type="SWC", cohort = NULL, draw = TRUE) {
-  type = match.arg(type, c("SWC", "E", "ETR", "WP"))
+spwb_validation<-function(x, measuredData, type="SWC", cohort = NULL, draw = TRUE,
+                          plotType = "dynamics") {
+  scatterplot<-function(df, xlab="", ylab="") {
+    g<-ggplot(df, aes(x=Modelled, y = Observed))+
+      geom_point()+
+      geom_abline(intercept=0, slope=1, col="black")+
+      geom_smooth(method="lm", se = FALSE, col="gray", linetype="dashed")+
+      xlab(xlab)+
+      ylab(ylab)+
+      theme_bw()
+    return(g)
+  }
+  dynamicsplot<-function(df, xlab="", ylab="") {
+    g<-ggplot(df, aes(x=Dates))+
+      geom_path(aes(y=Observed, col="Observed"))+
+      geom_path(aes(y=Modelled, col="Modelled"))+
+      xlab(xlab)+
+      ylab(ylab)+
+      scale_color_manual(name="", values=c("Observed"="black", "Modelled"= "red"))+
+      theme_bw()
+    return(g)
+  }
   evalstats<-function(obs, pred) {
     E <- pred-obs
     Bias <- mean(E, na.rm=T)
@@ -7,25 +27,94 @@ spwb_validation<-function(x, measuredData, type="SWC", cohort = NULL, draw = TRU
     R2<- cor(obs, pred, use="complete")^2
     return(c(n = sum(!is.na(obs) & !is.na(pred)), Bias= Bias, MAE = MAE, R2 = R2))
   }
+  
+  # Check arguments
+  type = match.arg(type, c("SWC", "SWC_scaled","E", "ETR", "WP"))
+  plotType = match.arg(plotType, c("dynamics", "scatter"))
+
   if(type=="SWC") {
     sm = x$Soil
     d = rownames(sm)
+    fc = soil_thetaFC(x$soilInput, model = x$spwbInput$control$soilFunctions)
+    df <- data.frame(Observed = NA, Modelled = sm$W.1*fc[1], Dates = as.Date(d))
+    
+    if(!("SWC" %in% names(measuredData))) stop(paste0("Column 'SWC' not found in measured data frame."))
     seld = rownames(measuredData) %in% d
+    df$Observed[d %in% rownames(measuredData)] = measuredData$SWC[seld]
+  
+    if(draw) {
+      if(plotType=="dynamics") {
+        g<-dynamicsplot(df, ylab = "Soil moisture (% vol)")
+      } else {
+        g<-scatterplot(df, xlab  = "Modelled soil moisture (% vol)",
+                       ylab = "Measured soil moisture (% vol)")
+      }
+      print(evalstats(df$Observed, df$Modelled))
+      return(g)
+    } else{
+      print(evalstats(df$Observed, df$Modelled))
+    }
+  } 
+  else if(type=="SWC_scaled") {
+    sm = x$Soil
+    d = rownames(sm)
     fc = soil_thetaFC(x$soilInput, model = x$spwbInput$control$soilFunctions)
     q_mod = quantile(sm$W.1, p=c(0.05,0.95), na.rm=T)
-    sm_mod = (sm$W.1-q_mod[1])/(q_mod[2]-q_mod[1])
+    df <- data.frame(Observed = NA, Modelled = (sm$W.1-q_mod[1])/(q_mod[2]-q_mod[1]), Dates = as.Date(d))
+    
+    if(!("SWC" %in% names(measuredData))) stop(paste0("Column 'SWC' not found in measured data frame."))
+    seld = rownames(measuredData) %in% d
     q_obs = quantile(measuredData$SWC[seld], p=c(0.05,0.95), na.rm=T)
-    sm_obs = (measuredData$SWC[seld]-q_obs[1])/(q_obs[2]-q_obs[1])
+    df$Observed[d %in% rownames(measuredData)] =(measuredData$SWC[seld]-q_obs[1])/(q_obs[2]-q_obs[1])
+    
     if(draw) {
-      par(mfrow=c(2,1), mar=c(4,4,1,1))
-      plot(as.Date(rownames(sm)), sm$W.1*fc[1], type="l", xlab="", ylab="Soil moisture 0-30 cm (% vol)", ylim=c(0,1))
-      lines(as.Date(rownames(sm)), measuredData$SWC[seld], col="red")
-      legend("topright", legend =c("Measured", "Modelled"), lty=1, col=c("red", "black"), bty="n")
-      plot(as.Date(rownames(sm)), sm_mod, type="l", xlab="", ylab="Soil moisture 0-30 cm (scaled)", ylim=c(0,1))
-      lines(as.Date(rownames(sm)), sm_obs, col="red")
+      if(plotType=="dynamics") {
+        g<-dynamicsplot(df, ylab = "Soil moisture (scaled)")
+      } else {
+        g<-scatterplot(df, xlab  = "Modelled soil moisture (scaled)",
+                       ylab = "Measured soil moisture (scaled)")
+
+      }
+      print(evalstats(df$Observed, df$Modelled))
+      return(g)
+    } else{
+      print(evalstats(df$Observed, df$Modelled))
     }
-    return(evalstats(sm_obs, sm_mod))
   } 
+  else if(type=="E") {
+    pt = x$PlantTranspiration
+    d = rownames(pt)
+    LAI = x$spwbInput$above$LAI_live
+    spnames = x$spwbInput$cohorts$Name
+    allcohnames = row.names(x$spwbInput$cohorts)
+    
+    if(is.null(cohort)) {
+      icoh = 1
+      cohort = allcohnames[1] 
+      message("Choosing first cohort")
+    } else {
+      icoh = which(allcohnames==cohort)
+    }
+    df <- data.frame(Observed = NA, Modelled = pt[,icoh]/LAI[icoh], Dates = as.Date(d))
+    ## Fill observed values
+    obscolumn = paste0("E_", cohort)
+    if(!(obscolumn %in% names(measuredData))) stop(paste0("Column '", obscolumn, "' not found in measured data frame."))
+    df$Observed[d %in% rownames(measuredData)] = measuredData[[obscolumn]][rownames(measuredData) %in% d] 
+    
+    if(draw) {
+      if(plotType=="dynamics") {
+        g<-dynamicsplot(df, ylab = "Transpiration per leaf area (mm/day)")
+      } else {
+        g<-scatterplot(df, 
+                       xlab = "Modelled transpiration per leaf area (mm/day)",
+                       ylab = "Measured transpiration per leaf area (mm/day)")
+      }
+      print(evalstats(df$Observed, df$Modelled))
+      return(g)
+    } else{
+      return(evalstats(df$Observed, df$Modelled))
+    }
+  }
   else if(type=="ETR") {
     ET1 = x$WaterBalance$SoilEvaporation+x$WaterBalance$Transpiration
     ET2 = x$WaterBalance$Evapotranspiration
@@ -60,64 +149,32 @@ spwb_validation<-function(x, measuredData, type="SWC", cohort = NULL, draw = TRU
     LAI = x$spwbInput$above$LAI_live
     spnames = x$spwbInput$cohorts$Name
     allcohnames = row.names(x$spwbInput$cohorts)
-    seld = rownames(measuredData) %in% d
     
-    if(!is.null(cohort)) {
-      obscolumn = paste0("E_", cohort)
-      icoh = which(allcohnames==cohort)
-      
-      E_mod = pt[,icoh]/LAI[icoh]
-      E_obs = measuredData[[obscolumn]][seld]
-      Emax = max(c(E_mod, E_obs), na.rm=T)
-      if(draw) {
-        par(mfrow=c(1,2), mar=c(4,4,1,1))
-        plot(as.Date(rownames(pt)), E_mod, type="l", ylab="Transpiration per leaf area (mm/day)", xlab="",
-             ylim=c(0,Emax))
-        lines(as.Date(row.names(measuredData))[seld], E_obs, col="red")
-        legend("topright", legend =c("Measured", "Modelled"), lty=1, col=c("red", "black"), bty="n")
-        plot(E_mod, E_obs, cex=0.5, pch = 19,
-             xlab ="Modelled transpiration", ylab="Measured transpiration", 
-             asp=1, xlim=c(0,Emax), ylim=c(0,Emax))
-        abline(a=0, b=1)
-        abline(lm(E_obs~E_mod), col="gray", lty=2)
-      }
-      return(evalstats(E_obs, E_mod))
+    if(is.null(cohort)) {
+      icoh = 1
+      cohort = allcohnames[1] 
+      message("Choosing first cohort")
     } else {
-      obscolumns = paste0("E_", allcohnames)
-      selc = obscolumns %in% names(measuredData)
-      cohorts = allcohnames[selc]
-      spnames = spnames[selc]
-      obscolumns = obscolumns[selc]
-      df = data.frame(n = rep(NA, length(cohorts)),
-                      Bias = rep(NA, length(cohorts)),
-                      MAE = rep(NA, length(cohorts)),
-                      R2 = rep(NA, length(cohorts)))
-      row.names(df)<-cohorts
-      if(draw)  par(mfrow=c(length(cohorts),2), mar=c(4,4,4,1))
-      for(i in 1:length(cohorts)) {
-        icoh = which(allcohnames==cohorts[i])
-        obscolumn = obscolumns[i]
-        E_mod = pt[,icoh]/LAI[icoh]
-        E_obs = measuredData[[obscolumn]][seld]
-        Emax = max(c(E_mod, E_obs), na.rm=T)
-        if(sum(!is.na(E_obs))>0) {
-          if(draw) {
-            plot(as.Date(rownames(pt)), E_mod, type="l", ylab="Transpiration per leaf area (mm/day)", xlab="",
-                 main = paste0(cohorts[i], " (",spnames[i],")"), ylim=c(0,Emax))
-            lines(as.Date(row.names(measuredData))[seld], E_obs, col="red")
-            legend("topright", legend =c("Measured", "Modelled"), lty=1, col=c("red", "black"), bty="n")
-            plot(E_mod, E_obs, cex=0.5, pch = 19,
-                 xlab ="Modelled transpiration per leaf area", ylab="Measured transpiration per leaf area", 
-                 asp=1, xlim=c(0,Emax), ylim=c(0,Emax))
-            abline(a=0, b=1)
-            abline(lm(E_obs~E_mod), col="gray", lty=2)
-          }
-          df[i,] = evalstats(E_mod, E_obs)
-        } else {
-          message(paste0("Not enough observations for ", cohorts[i]))
-        }
+      icoh = which(allcohnames==cohort)
+    }
+    df <- data.frame(Observed = NA, Modelled = pt[,icoh]/LAI[icoh], Dates = as.Date(d))
+    ## Fill observed values
+    obscolumn = paste0("E_", cohort)
+    if(!(obscolumn %in% names(measuredData))) stop(paste0("Column '", obscolumn, "' not found in measured data frame."))
+    df$Observed[d %in% rownames(measuredData)] = measuredData[[obscolumn]][rownames(measuredData) %in% d] 
+    
+    if(draw) {
+      if(plotType=="dynamics") {
+        g<-dynamicsplot(df, ylab = "Transpiration per leaf area (mm/day)")
+      } else {
+        g<-scatterplot(df, 
+                       xlab = "Modelled transpiration per leaf area (mm/day)",
+                       ylab = "Measured transpiration per leaf area (mm/day)")
       }
-      return(df)
+      print(evalstats(df$Observed, df$Modelled))
+      return(g)
+    } else{
+      return(evalstats(df$Observed, df$Modelled))
     }
   }
   else if(type=="WP"){
