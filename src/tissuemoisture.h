c754eae173380ed86ae3a53e906d0789e1f5f93f
miquelcaceres <miquelcaceres@gmail.com>
2020-11-30 08:35:50 +0100

    clean

--- src/tissuemoisture.h
+++ src/tissuemoisture.h
@@ -13,7 +13,6 @@ double apoplasticWaterPotential(double RWC, double c, double d);
 double segmentRelativeWaterContent(double psiSym, double pi0, double epsilon, 
                                   double psiApo, double c, double d, 
                                   double af);
-double tissueFMC(double RWC, double density, double d0 = 1.54);
 double tissueRelativeWaterContent(double psiSym, double pi0, double epsilon, 
                                   double psiApo, double c, double d, 
                                   double af, double femb = 0.0);
