075545ba0a3559ebeea7a7900bef29384d7d98fc
miquelcaceres <miquelcaceres@gmail.com>
2021-01-06 19:11:58 +0100

    multiple runs

--- R/optimization.R
+++ R/optimization.R
@@ -1,14 +1,37 @@
-optimization_evaluation_function<-function(x, soil, parNames, 
-                                           measuredData, type = "SWC", cohort = NULL, SpParams = NULL, 
-                                           metric = "loglikelihood",
-                                           ...) {
-  l = list(...)
-  if(!("meteo" %in% names(l))) stop("Please specify 'meteo' in '...'")
-  if(!("latitude" %in% names(l))) stop("Please specify 'latitude' in '...'")
+multiple_runs<-function(parMatrix, x, soil,
+                        meteo, latitude,
+                        elevation = NA, slope = NA, aspect = NA, 
+                        summary_function = NULL, args = NULL) {
+  if(inherits(x, "spwbInput")) model = "spwb"
+  else model = "growth"
   
-  if(!("elevation" %in% names(l))) l[["elevation"]] = NA
-  if(!("aspect" %in% names(l))) l[["aspect"]] = NA
-  if(!("slope" %in% names(l))) l[["slope"]] = NA
+  n = nrow(parMatrix)
+  res = vector("list", n)
+  parNames = colnames(parMatrix)
+  for(r in 1:n) {
+    resetInputs(x, soil)
+    customParams = parMatrix[r,]
+    names(customParams) <- parNames
+    x = modifyInputParams(x, customParams, soil)
+    x$control$verbose = FALSE
+    S = do.call(model, list(x = x, soil = soil, 
+                            meteo = meteo, 
+                            latitude = latitude, elevation = elevation,
+                            slope  = slope,aspect = aspect))
+    if(!is.null(summary_function)) {
+      res[[r]] = do.call(summary_function, c(list(S), args))
+    } else {
+      res[[r]] = S
+    }
+  }
+  return(res)
+}
+
+optimization_function<-function(parNames, x, soil,  
+                                meteo, latitude,
+                                elevation = NA, slope = NA, aspect = NA, 
+                                summary_function, args= NULL) {
+  
   if(inherits(x, "spwbInput")) model = "spwb"
   else model = "growth"
   
@@ -16,65 +39,44 @@ optimization_evaluation_function<-function(x, soil, parNames,
   
   yf<-function(v) {
     resetInputs(x, soil)
-    customParams = v
-    names(customParams) <- parNames
-    x = modifyInputParams(x, customParams, soil)
-    # print(x$below)
-    if(model=="spwb") {
-      S = spwb(x, soil, 
-               meteo = l[["meteo"]], 
-               latitude = l[["latitude"]], elevation = l[["elevation"]],
-               slope  = l[["slope"]],aspect = l[["aspect"]])
-    } 
-    else {
-      S = growth(x, soil, 
-                 meteo = l[["meteo"]], 
-                 latitude = l[["latitude"]], elevation = l[["elevation"]],
-                 slope  = l[["slope"]], aspect = l[["aspect"]])
+    if(is.vector(v)) {
+      customParams = v
+      names(customParams) <- parNames
+      x = modifyInputParams(x, customParams, soil)
+      S = do.call(model, list(x = x, soil = soil, 
+                              meteo = meteo, 
+                              latitude = latitude, elevation = elevation,
+                              slope  = slope,aspect = aspect))
+      y = do.call(summary_function, c(list(S), args))
+      return(y)
+    } else if(is.matrix(v)) {
+      colnames(v)<-parNames
+      y<-multiple_runs(parMatrix = v, x = x, soil = soil, 
+                       meteo = meteo,
+                       latitude = latitude, elevation = elevation,
+                       slope  = slope,aspect = aspect,
+                       summary_function = summary_function, args = args)
+      return(as.numeric(y))
+    } else {
+      stop("Wrong 'v' class")
     }
-    y = evaluation_metric(S, measuredData = measuredData, type=type, 
-                           cohort=cohort, SpParams = SpParams, metric = metric)
-    return(y)
   }
   return(yf)
 }
 
-optimization_prediction_function<-function(x, soil, parNames, 
-                                           summary_function,
-                                           ...) {
-  l = list(...)
-  if(!("meteo" %in% names(l))) stop("Please specify 'meteo' in '...'")
-  if(!("latitude" %in% names(l))) stop("Please specify 'latitude' in '...'")
-  
-  if(!("elevation" %in% names(l))) l[["elevation"]] = NA
-  if(!("aspect" %in% names(l))) l[["aspect"]] = NA
-  if(!("slope" %in% names(l))) l[["slope"]] = NA
-  if(inherits(x, "spwbInput")) model = "spwb"
-  else model = "growth"
-  
-  x$control$verbose = FALSE
-  
-  yf<-function(v) {
-    resetInputs(x, soil)
-    customParams = v
-    names(customParams) <- parNames
-    x = modifyInputParams(x, customParams, soil)
-    # print(x$below)
-    if(model=="spwb") {
-      S = spwb(x, soil, 
-               meteo = l[["meteo"]], 
-               latitude = l[["latitude"]], elevation = l[["elevation"]],
-               slope  = l[["slope"]],aspect = l[["aspect"]])
-    } 
-    else {
-      S = growth(x, soil, 
-                 meteo = l[["meteo"]], 
-                 latitude = l[["latitude"]], elevation = l[["elevation"]],
-                 slope  = l[["slope"]], aspect = l[["aspect"]])
-    }
-    y = summary_function(S)
-    # print(ll)
+optimization_evaluation_function<-function(parNames, x, soil, 
+                                           meteo, latitude,
+                                           elevation = NA, slope = NA, aspect = NA, 
+                                           measuredData, type = "SWC", cohort = NULL, SpParams = NULL, 
+                                           metric = "loglikelihood") {
+  sf<-function(S) {
+    y = evaluation_metric(S, measuredData = measuredData, type=type, 
+                           cohort=cohort, SpParams = SpParams, metric = metric)
     return(y)
   }
-  return(yf)
+  return(optimization_function(parNames = parNames, x = x, soil = soil,
+                               meteo = meteo, latitude = latitude,
+                               elevation = elevation, slope = slope, aspect = aspect,
+                               summary_function = sf))
 }
+
