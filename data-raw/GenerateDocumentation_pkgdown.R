e799cb3e9ce366e4a41a1e8a730bc9850c4dc90a
Miquel De Cáceres <miquelcaceres@gmail.com>
2021-07-07 11:23:56 +0200

    update parameterization spain

--- data-raw/GenerateDocumentation_pkgdown.R
+++ data-raw/GenerateDocumentation_pkgdown.R
@@ -1,8 +1,2 @@
 ## Script to update online documentation of the package
-## Result is stored in directory specified by 'destination' in file '_pkgdown.yml' 
-pkgdown::build_site()
-# pkgdown::build_reference(preview = FALSE)
-# pkgdown::build_reference_index()
-# pkgdown::build_news(preview = FALSE)
-# pkgdown::build_articles(preview = FALSE)
-# pkgdown::build_home()
+pkgdown::deploy_to_branch()
