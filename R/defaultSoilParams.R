ab3e9de4a3e752d8f9209465634e7435587367e6
Miquel De Cáceres <miquelcaceres@gmail.com>
2022-11-13 16:39:00 +0100

    roxygen

--- R/defaultSoilParams.R
+++ R/defaultSoilParams.R
@@ -1,3 +1,31 @@
+#' Default soil parameters
+#'
+#' Creates a data frame with default soil physical description for model functions
+#' 
+#' @param n An integer with the number of soil layers (between two and five).
+#' 
+#' @details The function returns a data frame with default physical soil description, with soil layers in rows. 
+#' Users can change those that need to be set to other values and use the list as input for function \code{\link{soil}}.
+#' 
+#' @note While this function is limited to five soil layers, user defined data frames can discretize soils using an unlimited number of soil layers.
+#' 
+#' @return A data frame with layers in rows and the following columns (and default values):
+#' \itemize{
+#'   \item{\code{widths (= c(300,700,1000,2000)}: Width of soil layers (in mm).}
+#'   \item{\code{clay (= 25)}: Clay percentage for each layer (in \%).}
+#'   \item{\code{sand (= 25)}: Sand percentage for each layer (in \%).}
+#'   \item{\code{om (= NA)}: Organic matter percentage for each layer (in \%).}
+#'   \item{\code{bd (= 1.5)}: Bulk density for each layer (in g/cm3).}
+#'   \item{\code{rfc (= c(20,40,60,85))}: Percentage of rock fragment content (volume basis) for each layer.}
+#' }
+#' 
+#' @author Miquel De \enc{Cáceres}{Caceres} Ainsa, CREAF
+#' 
+#' @seealso  \code{\link{soil}},  \code{\link{defaultControl}}, \code{\link{SpParamsMED}}
+#' 
+#' @examples 
+#' defaultSoilParams(4)
+#' 
 defaultSoilParams<-function(n=4) {
   n = min(max(n,2),5)
   return(data.frame(
