df2119ee071a459cc2d6ce2d37e680684612f79f
Miquel De Cáceres <miquelcaceres@gmail.com>
2024-10-10 17:36:55 +0200

    Add profiling sperry

--- tests/test_profiling.R
+++ tests/test_profiling.R
@@ -6,6 +6,16 @@ data(SpParamsMED)
 examplesoil <- defaultSoilParams(4)
 control <- defaultControl("Granier")
 x1 <- spwbInput(exampleforest,examplesoil, SpParamsMED, control)
-start_profiler("/tmp/profile.out")
+start_profiler("/tmp/profile_granier.out")
 S1 <- spwb(x1, examplemeteo, latitude = 41.82592, elevation = 100)
 stop_profiler()
+
+control <- defaultControl("Sperry")
+x2 <- spwbInput(exampleforest,examplesoil, SpParamsMED, control)
+start_profiler("/tmp/profile_sperry.out")
+S2 <- spwb(x2, examplemeteo, latitude = 41.82592, elevation = 100)
+stop_profiler()
+
+
+# google-pprof --gv /usr/bin/r /tmp/profile_granier.out 
+# google-pprof --gv /usr/bin/r /tmp/profile_sperry.out 
