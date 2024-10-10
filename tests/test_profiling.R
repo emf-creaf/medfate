1db921c15b65b14a1665484870c6cbb3458d0167
Miquel De Cáceres <miquelcaceres@gmail.com>
2024-10-10 19:23:29 +0200

    profiling sureau

--- tests/test_profiling.R
+++ tests/test_profiling.R
@@ -16,6 +16,13 @@ start_profiler("/tmp/profile_sperry.out")
 S2 <- spwb(x2, examplemeteo, latitude = 41.82592, elevation = 100)
 stop_profiler()
 
+control <- defaultControl("Sureau")
+x3 <- spwbInput(exampleforest,examplesoil, SpParamsMED, control)
+start_profiler("/tmp/profile_sureau.out")
+S3 <- spwb(x3, examplemeteo, latitude = 41.82592, elevation = 100)
+stop_profiler()
+
 
 # google-pprof --gv /usr/bin/r /tmp/profile_granier.out 
 # google-pprof --gv /usr/bin/r /tmp/profile_sperry.out 
+# google-pprof --gv /usr/bin/r /tmp/profile_sureau.out 
