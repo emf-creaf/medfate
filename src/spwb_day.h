42f3740a1ecf20eef2b5da92a8bd78d295888654
Miquel De Cáceres <miquelcaceres@gmail.com>
2024-10-26 09:15:54 +0200

    inner spwb and growth with files

---
+++ src/spwb_day.h
@@ -0,0 +1,22 @@
+#include <Rcpp.h>
+
+#ifndef SPWB_DAY_H
+#define SPWB_DAY_H
+#endif
+using namespace Rcpp;
+
+NumericVector fccsHazard(List x, NumericVector meteovec, List transp, double slope);
+
+List spwbDay(List x, CharacterVector date, NumericVector meteovec,
+             double latitude, double elevation, double slope = NA_REAL, double aspect = NA_REAL,  
+             double runon = 0.0, Nullable<NumericVector> lateralFlows = R_NilValue, double waterTableDepth = NA_REAL,
+             bool modifyInput = true);
+List spwbDay_basic(List internalCommunication, List x, NumericVector meteovec, 
+                   double elevation, double slope, double aspect,
+                   double runon = 0.0, Nullable<NumericVector> lateralFlows = R_NilValue, double waterTableDepth = NA_REAL, 
+                   bool verbose = false);
+List spwbDay_advanced(List internalCommunication, List x, NumericVector meteovec, 
+                      double latitude, double elevation, double slope, double aspect,
+                      double solarConstant, double delta, 
+                      double runon = 0.0, Nullable<NumericVector> lateralFlows = R_NilValue, double waterTableDepth = NA_REAL, 
+                      bool verbose = false);
