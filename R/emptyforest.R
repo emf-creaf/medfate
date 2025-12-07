4c8675b7beb0f4376a883574deb4313de63f8050
Miquel De Cáceres <miquelcaceres@gmail.com>
2025-12-07 09:56:32 +0100

    update forest man and emptyforest

--- R/emptyforest.R
+++ R/emptyforest.R
@@ -2,7 +2,7 @@
 #' 
 #' Creates an empty \code{\link{forest}} object.
 #' 
-#' @param ntree,nshrub Number of tree and shrub cohorts, respectively.
+#' @param ntree,nshrub,nherb Number of tree, shrub and herb cohorts, respectively.
 #' @param nseedling Number of species in the seedling bank.
 #' @param nseed Number of species in the seed bank.
 #' @param addcolumns A character vector with additional columns. Currently allowed are (see \code{\link{forest}}):
@@ -18,7 +18,7 @@
 #' 
 #' @details
 #' List elements \code{treeData} and \code{shrubData} are always created, regardless of the number of cohorts. 
-#' In contrast, list elements \code{seedBank} and \code{seedlingBank} are only created if \code{nseed} and \code{nseedling} are non-zero, respectively.
+#' In contrast, list elements \code{herbData}, \code{seedBank} and \code{seedlingBank} are only created if \code{nherb}, \code{nseed} and \code{nseedling} are non-zero, respectively.
 #' 
 #' 
 #' @return An empty \code{\link{forest}} object.
@@ -38,10 +38,10 @@
 #' # Initializes with extra column for leaf area index (LAI)
 #' emptyforest(ntree = 2, nshrub = 1, addcolumns = "LAI")
 #' 
-#' # Initializes forest with 2 tree cohorts, 1 shrub cohort and 1 seedling element
-#' emptyforest(ntree = 2, nshrub = 1, nseedling = 1)
+#' # Initializes forest with 2 tree cohorts, 1 shrub cohort and 1 herbaceous cohort
+#' emptyforest(ntree = 2, nshrub = 1, nherb = 1)
 #' @name emptyforest
-emptyforest <- function(ntree = 0, nshrub = 0, nseedling = 0, nseed = 0, 
+emptyforest <- function(ntree = 0, nshrub = 0, nherb = 0, nseedling = 0, nseed = 0, 
                         addcolumns = NULL) {
   l <- list()
   l$treeData <- data.frame(Species=as.character(rep(NA, ntree)),
@@ -55,8 +55,14 @@ emptyforest <- function(ntree = 0, nshrub = 0, nseedling = 0, nseed = 0,
                             Cover = as.numeric(rep(NA, nshrub)), 
                             Z50 = as.numeric(rep(NA, nshrub)), 
                             Z95=as.numeric(rep(NA, nshrub)))
-  # l$herbCover <- NA
-  # l$herbHeight <- NA
+
+  if(nherb>0) {
+    l$herbData <- data.frame(Species=as.character(rep(NA, nshrub)), 
+                              Height=as.numeric(rep(NA, nshrub)), 
+                              Cover = as.numeric(rep(NA, nshrub)), 
+                              Z50 = as.numeric(rep(NA, nshrub)), 
+                              Z95=as.numeric(rep(NA, nshrub)))
+  }
   if(nseedling > 0) {
     l$seedlingBank <- data.frame(Species = as.character(rep(NA, nseedling)),
                                  Percent = as.numeric(rep(NA, nseedling)),
@@ -75,31 +81,38 @@ emptyforest <- function(ntree = 0, nshrub = 0, nseedling = 0, nseed = 0,
     if("Z100" %in% addcolumns) {
       l$treeData$Z100 <- as.numeric(rep(NA, ntree))
       l$shrubData$Z100 <- as.numeric(rep(NA, nshrub))
+      if(nherb>0) l$herbData$Z100 <- as.numeric(rep(NA, nherb))
       if(nseedling>0) l$seedlingBank$Z100 <- as.numeric(rep(NA, nseedling))
     }
     if("Age" %in% addcolumns) {
       l$treeData$Age <- as.numeric(rep(NA, ntree))
       l$shrubData$Age <- as.numeric(rep(NA, nshrub))
+      if(nherb>0) l$herbData$Age <- as.numeric(rep(NA, nherb))
     }
     if("LAI" %in% addcolumns) {
       l$treeData$LAI <- as.numeric(rep(NA, ntree))
       l$shrubData$LAI <- as.numeric(rep(NA, nshrub))
+      if(nherb>0) l$herbData$LAI <- as.numeric(rep(NA, nherb))
     }
     if("FoliarBiomass" %in% addcolumns) {
       l$treeData$FoliarBiomass <- as.numeric(rep(NA, ntree))
       l$shrubData$FoliarBiomass <- as.numeric(rep(NA, nshrub))
+      if(nherb>0) l$herbData$FoliarBiomass <- as.numeric(rep(NA, nherb))
     }
     if("FuelLoading" %in% addcolumns) {
       l$treeData$FuelLoading <- as.numeric(rep(NA, ntree))
       l$shrubData$FuelLoading <- as.numeric(rep(NA, nshrub))
+      if(nherb>0) l$herbData$FuelLoading <- as.numeric(rep(NA, nherb))
     }
     if("CrownRatio" %in% addcolumns) {
       l$treeData$CrownRatio <- as.numeric(rep(NA, ntree))
       l$shrubData$CrownRatio <- as.numeric(rep(NA, nshrub))
+      if(nherb>0) l$herbData$CrownRatio <- as.numeric(rep(NA, nherb))
     }
     if("ObsID" %in% addcolumns) {
       l$treeData$ObsID <- as.character(rep(NA, ntree))
       l$shrubData$ObsID <- as.character(rep(NA, nshrub))
+      if(nherb>0) l$herbData$ObsID <- as.numeric(rep(NA, nherb))
     }
   }
   
